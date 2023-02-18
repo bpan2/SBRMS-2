d5f3d52945bc17d831774f55b62772de6ff92056
Bing <bpan2@users.noreply.github.com>
2023-02-18 17:01:13 -0500

    on Supplier.java: 1. updated with the imports of androidx.room and removed imports for android.arch.persistence.room 2. 5 warnings are ignored which should be handled later.

--- app/src/main/java/com/sbrms/models/Entities/Supplier.java
+++ app/src/main/java/com/sbrms/models/Entities/Supplier.java
@@ -1,9 +1,10 @@
 package com.sbrms.models.Entities;
 
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.Ignore;
-import android.arch.persistence.room.PrimaryKey;
-import android.support.annotation.NonNull;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
 
 @Entity(tableName = "suppliers")
 public class Supplier {
