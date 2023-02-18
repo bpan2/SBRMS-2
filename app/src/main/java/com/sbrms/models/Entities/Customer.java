4283c636b989dd7abbb65137118c59447a194cd5
Bing <bpan2@users.noreply.github.com>
2023-02-18 16:50:40 -0500

    on Customer: updated imports and removed imports for Arch

--- app/src/main/java/com/sbrms/models/Entities/Customer.java
+++ app/src/main/java/com/sbrms/models/Entities/Customer.java
@@ -1,8 +1,9 @@
 package com.sbrms.models.Entities;
 
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.PrimaryKey;
-import android.support.annotation.NonNull;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 @Entity(tableName = "customers")
 public class Customer {
