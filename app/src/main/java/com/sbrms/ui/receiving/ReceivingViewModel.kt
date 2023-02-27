6a68a9e43b03d91f2906306f380bfb2bc3209f5d
Bing <bpan2@users.noreply.github.com>
2023-02-26 23:01:18 -0500

    Created fragments with their viewmodels for History, Receiving, Returned, Sales, Search modules

---
+++ app/src/main/java/com/sbrms/ui/receiving/ReceivingViewModel.kt
@@ -0,0 +1,7 @@
+package com.sbrms.ui.receiving
+
+import androidx.lifecycle.ViewModel
+
+class ReceivingViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/sbrms/ui/searching/SearchViewModel.kt
@@ -0,0 +1,7 @@
+package com.sbrms.ui.searching
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
